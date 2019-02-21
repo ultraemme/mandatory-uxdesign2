31b65e3a01a79a8d72928e7770c42af375ca0ece
ultraemme <e.larsson1994@gmail.com>
2019-02-21 15:12:02 +0100

    fixed accessability for quiz form after opening and closing menu

--- src/js/templates/quizMainStartedTemplate.js
+++ src/js/templates/quizMainStartedTemplate.js
@@ -1,6 +1,6 @@
 export default function quizMainStartedTemplate(round) {
   return `<h2 class="quiz__header" tabindex="0">Quiz ${round}</h2>
-    <form action="">
+    <form action="" tabindex="-2">
       
     </form>`
 }
--- src/js/templates/quizQuestionTemplate.js
+++ src/js/templates/quizQuestionTemplate.js
@@ -1,6 +1,6 @@
 export default function quizQuestionTemplate(question, questionNo) {
-  return `<div class="quiz__question-${questionNo}">
-            <div class="quiz__question-container" tabindex="0"><strong>Q${questionNo}.</strong> ${question}</div>
-            <div class="quiz__answers-container"></div>
+  return `<div class="quiz__question-${questionNo}" tabindex="-2">
+            <div class="quiz__question-container" tabindex="0"><strong tabindex="-2">Q${questionNo}.</strong> ${question}</div>
+            <div class="quiz__answers-container" tabindex="-2"></div>
           </div>`
 }
